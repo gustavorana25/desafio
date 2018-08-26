b80126ee1bcb9bb9dc874f1b568e55cb2f9960cb
Gustavo Raña <gustavorana25@gmail.com>
2018-08-26 17:35:41 -0300

    resolvendo erro de css ao dar build no projeto

---
+++ helpers/commons-chunk-config.js
@@ -0,0 +1,24 @@
+// resolve problema de css que nao carregava quando dava npm run build e start
+module.exports = (config, test = /\.css$/) => {
+  // Extend the default CommonsChunkPlugin config
+  config.plugins = config.plugins.map(plugin => {
+    if (
+      plugin.constructor.name === 'CommonsChunkPlugin' &&
+      typeof plugin.minChunks !== 'undefined'
+    ) {
+      const defaultMinChunks = plugin.minChunks
+      plugin.minChunks = (module, count) => {
+        // Move all styles to commons chunk so they can be extracted to a single file
+        if (module.resource && module.resource.match(test)) {
+          return true
+        }
+        // Use default minChunks for non-style modules
+        return typeof defaultMinChunks === 'function'
+          ? defaultMinChunks(module, count)
+          : count >= defaultMinChunks
+      }
+    }
+    return plugin
+  })
+  return config
+}
--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+const withSass = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
+const commonsChunkConfig = require('./helpers/commons-chunk-config');
+module.exports = withSass(withCSS({
+    webpack: config => commonsChunkConfig(config, /\.(sass|scss|css)$/)
+}));
